ea8fa5c3405e259c214742aa6cf4aa53f121e145
ebartlik <ebartlik@gmail.com>
2018-06-21 21:20:45 -0400

    9:20

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ $(document).ready(function () {
     $("button").on("click", function() {
         var button = $(this).attr(".btn");
         var input = $(this).attr(".input");
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" +
-          input + "7ce37766719e46c4b25ed2dc2837eab3";
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+          input + "&api-key=7ce37766719e46c4b25ed2dc2837eab3";
     });
 $.ajax({
   url: queryUrl,
   method: 'GET',
-}).done(function(result) {
+}).then(function(result) {
+    var response = result.data;
   console.log(result);
 }).fail(function(err) {
   throw err;
